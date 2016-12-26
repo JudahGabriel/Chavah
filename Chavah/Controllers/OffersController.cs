96261f46a257aca8a830e1f937d9452c6e466554
Judah Gabriel Himango <judahgabriel@gmail.com>
2016-12-26 13:14:30 -0600

    Offers controller for special music offers.

---
+++ Chavah/Controllers/OffersController.cs
@@ -0,0 +1,19 @@
+﻿using BitShuva.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace BitShuva.Controllers
+{
+    public class OffersController : RavenController
+    {
+        public FileResult KolYonahFreeDownload()
+        {
+            ChavahLog.Info(this.DbSession, "Free Kol Yonah download has been downloaded", Request.UserAgent);
+            var filePath = Server.MapPath("~/Content/offers/Micha'el Eliyahu BenDavid - Kol Yonah - 10 - Rejoice in Yah.mp3");
+            return File(filePath, "audio/mpeg", "Rejoice in Yah.mp3");
+        }
+    }
+}
