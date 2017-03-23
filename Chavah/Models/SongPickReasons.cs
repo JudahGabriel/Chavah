7ddbafe5a77b87212aa5b87a50b6884e97cc2870
Judah Gabriel Himango <judahgabriel@gmail.com>
2017-03-23 16:53:51 -0500

    MediaSession API implemented.

--- Chavah/Models/SongPickReasons.cs
+++ Chavah/Models/SongPickReasons.cs
@@ -9,33 +9,9 @@ namespace BitShuva.Models
     {
         public SongPickReasons()
         {
-            this.LikedTags = new List<string>();
-            this.LovedTags = new List<string>();
         }
 
         public string SongId { get; set; }
-
-        [Obsolete("Removed; delete after 3/21/17. Use Artist instead")]
-        public bool LikedArtist { get; set; }
-        [Obsolete("Removed; delete after 3/21/17. Use Artist instead")]
-        public bool LovedArtist { get; set; }
-        [Obsolete("Removed; delete after 3/21/17. Use Album instead")]
-        public bool LikedAlbum { get; set; }
-        [Obsolete("Removed; delete after 3/21/17. Use Album instead")]
-        public bool LovedAlbum { get; set; }
-        [Obsolete("Removed; delete after 3/21/17. Use SongThumbedUp instead")]
-        public bool LikedSong { get; set; }
-        [Obsolete("Removed; delete after 3/21/17. Use Ranking instead")]
-        public bool BestRanking { get; set; }
-        [Obsolete("Removed; delete after 3/21/17. Use Ranking instead")]
-        public bool GreatRanking { get; set; }
-        [Obsolete("Removed; delete after 3/21/17. Use Ranking instead")]
-        public bool GoodRanking { get; set; }
-        [Obsolete("Removed; delete after 3/21/17. Use Similar instead")]
-        public List<string> LikedTags { get; set; }
-        [Obsolete("Removed; delete after 3/21/17. Use Similar instead")]
-        public List<string> LovedTags { get; set; }
-
         public LikeLevel Artist { get; set; }
         public LikeLevel Album { get; set; }
         public bool SongThumbedUp { get; set; }
