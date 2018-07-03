adbcaf6a3e134f991bb4f87825ec5691215cc4f0
JudahGabriel <judahgabriel@gmail.com>
2018-07-03 14:34:04 -0500

    Background image directive, used in my likes->artists.

---
+++ Chavah.NetCore/wwwroot/js/Directives/BackgroundImage.ts
@@ -0,0 +1,38 @@
+﻿module BitShuva.Chavah {
+
+    /**
+     * Angular directive for setting the background image of an element.
+     */
+    class BackgroundImage {
+
+        constructor(scope: ng.IScope, private element: JQuery, attributes: ng.IAttributes, ngModelCtrl: ng.INgModelController) {
+            var backgroundImageUrl = scope.$eval(attributes["backgroundImage"]);
+            scope.$watch(attributes["backgroundImage"], (newVal: string) => this.applyBackgroundImage(newVal));
+        }
+
+        private applyBackgroundImage(imageUrl: string) {
+            if (imageUrl) {
+                var backgroundImageCssValue = `url("${imageUrl}")`;
+                this.element.css("background-image", backgroundImageCssValue);
+            } else {
+                this.element.css("background-image", "none");
+            }
+        }
+    }
+
+    export class BackgroundImageBinder implements ng.IDirective {
+        link: ng.IDirectiveLinkFn;
+        restrict = "A";
+        priority = 99;
+
+        constructor() {
+            this.link = this.unboundLink.bind(this);
+        }
+
+        unboundLink(scope: ng.IScope, element: JQuery, attributes: ng.IAttributes, ngModelCtrl: ng.INgModelController) {
+            new BackgroundImage(scope, element, attributes, ngModelCtrl);
+        }
+    }
+
+    App.directive("backgroundImage", () => new BackgroundImageBinder());
+}
