a7b7555275cd3459fe726c4753910d28fbd85849
Judah Gabriel Himango <judahgabriel@gmail.com>
2018-08-23 11:42:29 -0500

    Admin songs list with search, edit, delete.

---
+++ Chavah.NetCore/wwwroot/js/Controllers/ConfirmDeleteSongController.ts
@@ -0,0 +1,33 @@
+﻿namespace BitShuva.Chavah {
+    export class ConfirmDeleteSongController {
+        saving = false;
+
+        static $inject = [
+            "song",
+            "songApi",
+            "$uibModalInstance"
+        ];
+
+        constructor(
+            private readonly song: Song,
+            private readonly songApi: SongApiService,
+            private $uibModalInstance: ng.ui.bootstrap.IModalServiceInstance) {
+
+        }
+
+        close() {
+            this.$uibModalInstance.close(false);
+        }
+
+        deleteSong() {
+            if (!this.saving) {
+                this.saving = true;
+                this.songApi.deleteSong(this.song)
+                    .then(() => this.$uibModalInstance.close(true))
+                    .finally(() => this.saving = false);
+            }
+        }
+    }
+
+    App.controller("ConfirmDeleteSongController", ConfirmDeleteSongController);
+}
