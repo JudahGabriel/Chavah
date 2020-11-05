55b2c03e1181253f9b59be702b0537afffa96f5d
Judah Gabriel Himango <judahgabriel@gmail.com>
2020-11-05 15:11:31 -0800

    Enhancements the admin users page

--- Chavah.NetCore/wwwroot/js/Controllers/AdminUsersController.ts
+++ Chavah.NetCore/wwwroot/js/Controllers/AdminUsersController.ts
@@ -1,6 +1,16 @@
 ﻿namespace BitShuva.Chavah {
     export class AdminUsersController {
 
+        private timeframes: ToggleOption[] = [
+            { title: "Last week", value: 7 },
+            { title: "Last month", value: 30 },
+            { title: "Last 3 months", value: 30 * 3 },
+            { title: "Last 6 months", value: 30 * 6 },
+            { title: "Last year", value: 365 },
+            { title: "Last 5 years", value: 365 * 5 }
+        ];
+        private activeTimeframe = this.timeframes[0];
+
         static $inject = [
             "userApi",
             "adminScripts"
@@ -12,10 +22,8 @@
         }
 
         $onInit() {
-            const loadChartsTask = this.loadGoogleCharts();
-            const yearAgo = moment.utc().subtract(6, "months");
-            const getUsersTask = this.userApi.getRegistrations(yearAgo.toISOString());
-            Promise.all([getUsersTask, loadChartsTask]).then(results => this.drawChart(results[0]));
+            const loadChartsTask = this.loadGoogleCharts()
+                .then(() => this.changeTimeframe(this.timeframes[0]));
         }
 
         loadGoogleCharts(): Promise<void> {
@@ -33,8 +41,8 @@
             const chartData = this.usersToChartData(users);
             var options = {
                 chart: {
-                    title: 'Total registered users',
-                    subtitle: 'over the last 6 months'
+                    title: `${users.items.length} new users`,
+                    subtitle: `in the ${this.activeTimeframe.title.toLowerCase()}`
                 },
                 width: 900,
                 height: 700
@@ -42,6 +50,13 @@
 
             const chart = new google.charts["Line"](document.querySelector("#adminUsersChart"));
             chart.draw(chartData, google.charts["Line"].convertOptions(options));
+        }
+
+        changeTimeframe(timeframe: ToggleOption) {
+            this.activeTimeframe = timeframe;
+            const timeAgo = moment.utc().subtract(timeframe.value as number, "days");
+            const getUsersTask = this.userApi.getRegistrations(timeAgo.toISOString());
+            getUsersTask.then(users => this.drawChart(users));
         }
 
         usersToChartData(users: Server.PagedList<Server.User>): google.visualization.DataTable {
